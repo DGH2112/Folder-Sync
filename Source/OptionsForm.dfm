1b6c155225caff5289e95f7ed85312a83ddc5243
DGH2112 <davidghoyle@gmail.com>
2004-10-02 17:08:36 +0100

    Updated to new version of Folder Sync

--- Source/OptionsForm.dfm
+++ Source/OptionsForm.dfm
@@ -1,10 +1,10 @@
 object frmOptions: TfrmOptions
-  Left = 548
-  Top = 415
+  Left = 502
+  Top = 310
   BorderStyle = bsDialog
   Caption = 'Folder Sync Options'
-  ClientHeight = 297
-  ClientWidth = 483
+  ClientHeight = 444
+  ClientWidth = 659
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -16,8 +16,8 @@ object frmOptions: TfrmOptions
   PixelsPerInch = 96
   TextHeight = 13
   object lblTimeDiff: TLabel
-    Left = 4
-    Top = 272
+    Left = 8
+    Top = 420
     Width = 117
     Height = 13
     Caption = '&Time Different Tolerance'
@@ -28,58 +28,112 @@ object frmOptions: TfrmOptions
     Font.Style = []
     ParentFont = False
   end
-  object Label1: TLabel
-    Left = 4
-    Top = 4
-    Width = 50
-    Height = 13
-    Caption = '&Exclusions'
-    FocusControl = edtExclusions
-  end
-  object edtExclusions: TMemo
-    Left = 4
-    Top = 24
-    Width = 473
-    Height = 237
-    ScrollBars = ssBoth
-    TabOrder = 0
-    WordWrap = False
-  end
   object edtTimeDiff: TEdit
-    Left = 136
-    Top = 268
+    Left = 140
+    Top = 416
     Width = 37
     Height = 21
     ReadOnly = True
-    TabOrder = 1
+    TabOrder = 0
     Text = '0'
   end
   object udTolerance: TUpDown
-    Left = 173
-    Top = 268
+    Left = 177
+    Top = 416
     Width = 16
     Height = 21
     Associate = edtTimeDiff
     Min = 0
     Max = 60
     Position = 0
-    TabOrder = 2
+    TabOrder = 1
     Wrap = False
   end
   object btnOK: TBitBtn
-    Left = 324
-    Top = 268
+    Left = 500
+    Top = 416
     Width = 75
     Height = 25
-    TabOrder = 3
+    TabOrder = 2
     Kind = bkOK
   end
   object btnCancel: TBitBtn
-    Left = 404
-    Top = 268
+    Left = 580
+    Top = 416
     Width = 75
     Height = 25
-    TabOrder = 4
+    TabOrder = 3
     Kind = bkCancel
+  end
+  object PageControl1: TPageControl
+    Left = 8
+    Top = 8
+    Width = 645
+    Height = 405
+    ActivePage = pgFolderList
+    TabOrder = 4
+    object pgFolderList: TTabSheet
+      Caption = 'Folder List'
+      object lvFolders: TListView
+        Left = 4
+        Top = 4
+        Width = 629
+        Height = 337
+        Columns = <
+          item
+            Caption = 'Left Folder'
+            Width = 300
+          end
+          item
+            Caption = 'Right Folder'
+            Width = 300
+          end>
+        ReadOnly = True
+        RowSelect = True
+        TabOrder = 0
+        ViewStyle = vsReport
+        OnDblClick = lvFoldersDblClick
+      end
+      object btnAdd: TBitBtn
+        Left = 392
+        Top = 348
+        Width = 75
+        Height = 25
+        Caption = '&Add'
+        TabOrder = 1
+        OnClick = btnAddClick
+      end
+      object btnEdit: TBitBtn
+        Left = 476
+        Top = 348
+        Width = 75
+        Height = 25
+        Caption = '&Edit'
+        TabOrder = 2
+        OnClick = btnEditClick
+      end
+      object btnDelete: TBitBtn
+        Left = 560
+        Top = 348
+        Width = 75
+        Height = 25
+        Caption = '&Delete'
+        TabOrder = 3
+        OnClick = btnDeleteClick
+      end
+    end
+    object pgExclusions: TTabSheet
+      Caption = '&Exclusions'
+      ImageIndex = 1
+      object edtExclusions: TMemo
+        Left = 4
+        Top = 8
+        Width = 629
+        Height = 365
+        ScrollBars = ssBoth
+        TabOrder = 0
+        WordWrap = False
+      end
+    end
   end
 end
--- Source/OptionsForm.pas
+++ Source/OptionsForm.pas
@@ -1,3 +1,12 @@
+(**
+  
+  This module defines the options dialogue.
+
+  @date    02 Oct 2004
+  @Version 1.0
+  @Author  David Hoyle
+  
+**)
 unit OptionsForm;
 
 interface
@@ -7,38 +16,80 @@ uses
   StdCtrls, Buttons, ComCtrls;
 
 type
+  (** This is a class to represent **)
   TfrmOptions = class(TForm)
-    edtExclusions: TMemo;
     lblTimeDiff: TLabel;
-    Label1: TLabel;
     edtTimeDiff: TEdit;
     udTolerance: TUpDown;
     btnOK: TBitBtn;
     btnCancel: TBitBtn;
+    PageControl1: TPageControl;
+    pgFolderList: TTabSheet;
+    pgExclusions: TTabSheet;
+    edtExclusions: TMemo;
+    lvFolders: TListView;
+    btnAdd: TBitBtn;
+    btnEdit: TBitBtn;
+    btnDelete: TBitBtn;
+    procedure btnAddClick(Sender: TObject);
+    procedure btnEditClick(Sender: TObject);
+    procedure btnDeleteClick(Sender: TObject);
+    procedure lvFoldersDblClick(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
-    Class Function Execute(var strExclusions :String;
+    Class Function Execute(var slFolders : TStringList; var strExclusions :String;
       var iTolerance : Integer) : Boolean;
   end;
 
 implementation
 
+uses FolderPathsForm;
+
 {$R *.DFM}
 
 { TfrmOptions }
 
-class function TfrmOptions.Execute(var strExclusions: String;
-  var iTolerance: Integer): Boolean;
+(**
+
+  This is the classes main interface method for editing the applications options.
+
+  @precon  None.
+  @postcon Returns true with the updated options in the var variables else
+           returns false if the dialogue is cancelled.
+
+  @param   slFolders     as a TStringList as a reference
+  @param   strExclusions as a String as a reference
+  @param   iTolerance    as an Integer as a reference
+  @return  a Boolean
+
+**)
+class function TfrmOptions.Execute(var slFolders : TStringList;
+  var strExclusions: String; var iTolerance: Integer): Boolean;
+
+Var
+  i : Integer;
+  Item : TListItem;
+
 begin
   Result := False;
   With TfrmOptions.Create(Nil) Do
     Try
+      For i := 0 to slFolders.Count - 1 Do
+        Begin
+          Item := lvFolders.Items.Add;
+          Item.Caption := slFolders.Names[i];
+          Item.SubItems.Add(slFolders.Values[slFolders.Names[i]]);
+        End;
       edtExclusions.Text := strExclusions;
       udTolerance.Position := iTolerance;
       If ShowModal = mrOK Then
         Begin
+          slFolders.Clear;
+          For i := 0 To lvFolders.Items.Count - 1 Do
+            slFolders.Add(lvFolders.Items[i].Caption + '=' +
+              lvFolders.Items[i].SubItems[0]);
           strExclusions := edtExclusions.Text;
           iTolerance := udTolerance.Position;
           Result := True;
@@ -46,6 +97,92 @@ begin
     Finally
       Free;
     End;
+end;
+
+(**
+
+  This is an on click event handler for the Add button.
+
+  @precon  None.
+  @postcon Adds the folder paths returned from the Folder Paths dialogue to the
+           list view.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmOptions.btnAddClick(Sender: TObject);
+
+Var
+  strLeft, strRight : String;
+  Item : TListItem;
+
+begin
+  If TfrmFolderPaths.Execute(strLeft, strRight) Then
+    Begin
+      Item := lvFolders.Items.Add;
+      Item.Caption := strLeft;
+      Item.SubItems.Add(strRight);
+    End;
+end;
+
+(**
+
+  This is an on click event handler for the Edit button.
+
+  @precon  None.
+  @postcon Allows the selected item to be edited in the Folder Paths dialogue.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmOptions.btnEditClick(Sender: TObject);
+
+Var
+  strLeft, strRight : String;
+
+begin
+  If lvFolders.Selected <> Nil Then
+    Begin
+      strLeft := lvFolders.Selected.Caption;
+      strRight := lvFolders.Selected.SubItems[0];
+      If TfrmFolderPaths.Execute(strLeft, strRight) Then
+        Begin
+          lvFolders.Selected.Caption := strLeft;
+          lvFolders.Selected.SubItems[0] := strRight;
+        End;
+    End;
+end;
+
+(**
+
+  This is an on click event handler for the delete button.
+
+  @precon  None.
+  @postcon Deletes the selected item from the folders list view.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmOptions.btnDeleteClick(Sender: TObject);
+
+begin
+  If lvFolders.Selected <> Nil Then
+    lvFolders.Selected.Delete;
+end;
+
+(**
+
+  This is an on double event handler for the list view.
+
+  @precon  None.
+  @postcon Edits the selected item in the list.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmOptions.lvFoldersDblClick(Sender: TObject);
+begin
+  btnEditClick(Self);
 end;
 
 end.
