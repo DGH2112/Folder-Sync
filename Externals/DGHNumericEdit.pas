8b2339f4bc82d57c8e95dcf651531607a7b1e572
DGH2112 <davidghoyle@gmail.com>
2007-10-06 11:11:17 +0100

    Added an intermediate class to allow edit controls to Align the text and behave as <Tabs> on <Enter>.

--- Externals/DGHNumericEdit.pas
+++ Externals/DGHNumericEdit.pas
@@ -1,3 +1,15 @@
+(**
+
+  This module contains 2 controls, the first a modified edit control with
+  the ability to tab forward to the next control if the <Enter> key is pressed
+  and allows the alignment of the text and a second control which inherited from
+  the first and allows the evaluation of numeric expressions.
+
+  @Version 1.0
+  @Author  David Hoyle
+  @Date    06 Oct 2007
+
+**)
 unit DGHNumericEdit;
 
 interface
@@ -6,48 +18,69 @@ uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   StdCtrls;
 
-{ -------------------------------------------------------------------------
-
-   This module defines a descendant of TEdit that allows for alignment
-   justment and the evauation of numerical inputs but does not evaluate the
-   input if there is an OnExit event handler assigned.
-
-   New Properties
-
-     Align         : TAlignment
-     DecimalPlaces : Integer
-     Value         : Double
-
-  -------------------------------------------------------------------------- }
-
 type
-  TNumericEdit = class(TEdit)
+  (** A new edit control with Alignment and EnterAsTab properties. **)
+  TDGHEdit = class(TEdit)
+  Private
+    FAlignment : TAlignment;
+    FEnterAsTab : Boolean;
+  Protected
+    Procedure SetAlignment(Value : TAlignment);
+    Procedure KeyPress(var Key: Char); Override;
+  Public
+    Procedure CreateParams(var Params: TCreateParams); Override;
+  Published
+    (**
+      This property sets the alignment of the text.
+      @precon  None.
+      @postcon Sets the alignment of the text.
+      @return  a TAlignment
+    **)
+    Property Alignment : TAlignment Read FAlignment Write SetAlignment;
+    (**
+      This property sets whether the Enter key behaves as a Tab key.
+      @precon  None.
+      @postcon Sets whether the Enter key behaves as a Tab key.
+      @return  a Boolean
+    **)
+    Property EnterAsTab : Boolean Read FEnterAsTab Write FEnterAsTab Default False;
+  end;
+
+  (** A new edit control with evaluation of numeric expressions. **)
+  TNumericEdit = class(TDGHEdit)
   private
     { Private declarations }
-    FAlignment : TAlignment;
     FDecimalPlaces : Integer;
     FValue : Double;
-    FEnterAsTab : Boolean;
     procedure SetDecimalPlaces(const Value: Integer);
     function GetValue: Double;
     procedure SetValue(const Value: Double);
   protected
     { Protected declarations }
-    Procedure SetAlignment(Value : TAlignment);
     Procedure DoExit; Override;
-    Procedure KeyPress(var Key: Char); Override;
   public
     { Public declarations }
     Constructor Create(AOwner : TComponent); Override;
-    Procedure CreateParams(var Params: TCreateParams); Override;
     Procedure EvaluateInput; Virtual;
+    (**
+      This property gets and sets the value of the expression.
+      @precon  None.
+      @postcon Gets and sets the value of the expression.
+      @return  a Double
+    **)
     Property Value : Double Read GetValue Write SetValue;
   published
     { Published declarations }
-    Property Alignment : TAlignment Read FAlignment Write SetAlignment;
+    (**
+      This property determines the number of decimal places of the expression
+      result.
+      @precon  None.
+      @postcon Determines the number of decimal places of the expression
+               result.
+      @return  an Integer
+    **)
     Property DecimalPlaces : Integer read FDecimalPlaces
       write SetDecimalPlaces Default 3;
-    Property EnterAsTab : Boolean Read FEnterAsTab Write FEnterAsTab Default False;
   end;
 
 procedure Register;
@@ -57,47 +90,32 @@ implementation
 Uses
   DGHLibrary;
 
-{ -------------------------------------------------------------------------
+(**
 
-   Standard component Register procedure.
+  The standard Delphi register procedure..
 
-  -------------------------------------------------------------------------- }
+  @precon  None
+  @postcon Registers the component with the Delphi Palette.
 
+**)
 procedure Register;
 begin
   RegisterComponents('DGH Controls', [TNumericEdit]);
 end;
 
-{ -------------------------------------------------------------------------
+(**
 
-   This is an overridden constructor for the component setting the number
-   of decimal places to 3 as default.
+  This is an overridden CreateParams method to allow the edit control's text
+  to be aligned.
 
-   Create(
-     AOwner   // Owner Component as TComponent
-   );
+  @precon  None.
+  @postcon Creates the control so that the style contains the alignment
+           information.
 
-  -------------------------------------------------------------------------- }
+  @param   Params as a TCreateParams as a reference
 
-Constructor TNumericEdit.Create(AOwner: TComponent);
-begin
-  Inherited Create(AOwner);
-  FDecimalPlaces := 3;
-  FValue := 0;
-end;
-
-{ -------------------------------------------------------------------------
-
-   This is an overridden CreateParams procedure to allow for the edit
-   control to be aligned left, centre or right.
-
-   CreateParams(
-     Params    // TCreateParams structure to update
-   );
-
-  -------------------------------------------------------------------------- }
-
-Procedure TNumericEdit.CreateParams(var Params: TCreateParams);
+**)
+Procedure TDGHEdit.CreateParams(var Params: TCreateParams);
 
 Var
   X : DWORD;
@@ -114,17 +132,77 @@ Begin
   Params.Style := Params.Style Or X;
 End;
 
-{ -------------------------------------------------------------------------
+(**
 
-   Overridden DoExit procedure that evaluate the input of the edit control
-   on the focus leaving. This does not evaluate the input if there is an
-   OnExit event handler assigned.
+  This is an overridden method to allow the enter key to be used as a Tab key to
+  move the focus to the next control on the form.
 
-   DoExit(
-   );
+  @precon  None.
+  @postcon If the property EnterAsTab is true then a tab keybaord event is
+           generated.
 
-  -------------------------------------------------------------------------- }
+  @param   Key as a Char as a reference
 
+**)
+procedure TDGHEdit.KeyPress(var Key : Char);
+begin
+  If FEnterAsTab Then
+    If Key = #13 Then
+      Begin
+        Key := #0;
+        keybd_event(VK_TAB, MapVirtualKey(VK_TAB, 0), 0, 0);
+        keybd_event(VK_TAB, MapVirtualKey(VK_TAB, 0), KEYEVENTF_KEYUP, 0);
+      End;
+  inherited;
+end;
+
+(**
+
+  This is a setter method for the Alignment property.
+
+  @precon  None.
+  @postcon Sets the alignment of the edit controls text.
+
+  @param   Value as a TAlignment
+
+**)
+Procedure TDGHEdit.SetAlignment(Value : TAlignment);
+
+Begin
+  If Value <> FAlignment Then
+    Begin
+      FAlignment := Value;
+      ReCreateWnd;
+    End;
+End;
+
+(**
+
+  This is the constructor method for the TNumericEdit class.
+
+  @precon  None;
+  @postcon Sets the default values of the component.
+
+  @param   AOwner as a TComponent
+
+**)
+Constructor TNumericEdit.Create(AOwner: TComponent);
+begin
+  Inherited Create(AOwner);
+  FDecimalPlaces := 3;
+  FValue := 0;
+end;
+
+(**
+
+  This is an overidden DoExit method to evaluate the numeric expression is an
+  on exit event handler is NOT assigned.
+
+  @precon  None.
+  @postcon Overidden DoExit method to evaluate the numeric expression is an
+           on exit event handler is NOT assigned.
+
+**)
 procedure TNumericEdit.DoExit;
 begin
   inherited;
@@ -132,15 +210,16 @@ begin
     EvaluateInput;
 end;
 
-{ -------------------------------------------------------------------------
+(**
 
-   This routine evalaute the text and returns the result.
+  This method attempts to evaluate the controls expression, if an exception
+  occurs it is handled and the focus remains in the control.
 
-   EvaluateInput(
-   );
+  @precon  None.
+  @postcon Attempts to evaluate the controls expression, if an exception
+           occurs it is handled and the focus remains in the control.
 
-  -------------------------------------------------------------------------- }
-
+**)
 procedure TNumericEdit.EvaluateInput;
 begin
   Try
@@ -154,15 +233,16 @@ begin
   End;
 end;
 
-{ -------------------------------------------------------------------------
+(**
 
-   Getter method for the Value property.
+  This is a getter method for the Value property.
 
-   GetValue(
-   );    // Returns the edit controls value as a double.
+  @precon  None.
+  @postcon Returns the value of the Value property.
 
-  -------------------------------------------------------------------------- }
+  @return  a Double
 
+**)
 function TNumericEdit.GetValue: Double;
 begin
   (*
@@ -185,48 +265,16 @@ begin
   Result := FValue;
 end;
 
-procedure TNumericEdit.KeyPress(var Key : Char);
-begin
-  If FEnterAsTab Then
-    If Key = #13 Then
-      Begin
-        Key := #0;
-        keybd_event(VK_TAB, MapVirtualKey(VK_TAB, 0), 0, 0);
-        keybd_event(VK_TAB, MapVirtualKey(VK_TAB, 0), KEYEVENTF_KEYUP, 0);
-      End;
-  inherited;
-end;
+(**
 
-{ -------------------------------------------------------------------------
+  This is a setter method for the DecimalPlaces property.
 
-   Setter method for the controls Align property.
+  @precon  None.
+  @postcon Sets the decimal places of the result.
 
-   SetAlignment(
-     Value    // A TAlignment value
-   );
+  @param   Value as an Integer constant
 
-  -------------------------------------------------------------------------- }
-
-Procedure TNumericEdit.SetAlignment(Value : TAlignment);
-
-Begin
-  If Value <> FAlignment Then
-    Begin
-      FAlignment := Value;
-      ReCreateWnd;
-    End;
-End;
-
-{ -------------------------------------------------------------------------
-
-   Setter method for the DecimalPlaces property.
-
-   SetDecimals(
-     Value   // Number of decimal places as an integer between 0..9
-   );
-
-  -------------------------------------------------------------------------- }
-
+**)
 procedure TNumericEdit.SetDecimalPlaces(const Value: Integer);
 begin
   If Value <> FDecimalPLaces Then
@@ -238,16 +286,17 @@ begin
     End;
 end;
 
-{ -------------------------------------------------------------------------
+(**
 
-   Setter method for the Value property.
+  This is a setter method for the Value property.
 
-   SetValue(
-     Value   // Value as a double
-   );
+  @precon  None.
+  @postcon Formats the Value using the FValue variable and the number of
+           decimal places.
 
-  -------------------------------------------------------------------------- }
+  @param   Value as a Double constant
 
+**)
 procedure TNumericEdit.SetValue(const Value: Double);
 begin
   FValue := Value;
