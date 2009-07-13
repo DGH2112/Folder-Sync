7a928daa229b3d24f1b7777e475e98f301966291
DGH2112 <davidghoyle@gmail.com>
2009-07-13 19:02:31 +0100

    Updated folder view with new files paths shown in grey.

--- FldrSync.dpr
+++ FldrSync.dpr
@@ -3,30 +3,13 @@
   The main delphi module for the Folder Synchronisation application.
 
   @Author  David Hoyle
-  @Date    12 Apr 2009
+  @Date    13 Jul 2009
   @Version 1.0
 
 **)
 program FldrSync;
 
-{%TogetherDiagram 'ModelSupport_FldrSync\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\MainForm\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\About\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\FileComparision\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\ProgressForm\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\FolderPathsForm\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\FldrSync\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\OptionsForm\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_FldrSync\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\FolderPathsForm\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\MainForm\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\FldrSync\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\FileComparision\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\About\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\ProgressForm\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\OptionsForm\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_FldrSync\Use Case Diagram1.txvuse'}
-{%TogetherDiagram 'ModelSupport_FldrSync\Sequence Diagram1.txvint'}
+
 
 uses
   ExceptionLog,
