d353e8c176c5cd1c3ea700f39331b95f97ef08e3
DGH2112 <davidghoyle@gmail.com>
2012-12-19 20:30:52 +0000

    Updated code to reflect themes.

--- Externals/DGHMemoryMonitorControl.pas
+++ Externals/DGHMemoryMonitorControl.pas
@@ -5,7 +5,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    01 Aug 2012
+  @Date    06 Dec 2012
 
 **)
 Unit DGHMemoryMonitorControl;
@@ -22,10 +22,10 @@ Uses
   DGHCustomGraphicsControl;
 
 Type
-    (** A class to represent the Battery Monitor Grasphics control. **)
+  (** A class to represent the Battery Monitor Grasphics control. **)
   TDGHMemoryMonitor = Class(TDGHCustomGraphicControl)
   Private
-      { Private declarations }
+    { Private declarations }
     FTimer         : TTimer;
     FUpdateInterval: Integer;
     FUsed          : Cardinal;
@@ -43,7 +43,7 @@ Type
     FHalfPoint     : TPercentage;
     FBackFontColour: TColor;
   Protected
-      { Protected declarations }
+    { Protected declarations }
     Procedure Paint; Override;
     Procedure SetUpdateInterval(Value: Integer); Virtual;
     Procedure SetLowPoint(Const Value: TPercentage); Virtual;
@@ -52,111 +52,111 @@ Type
     Procedure UpdateControl(Sender: TObject); Virtual;
     Procedure GetFFMemoryManagerState; Virtual;
   Public
-      { Public declarations }
+    { Public declarations }
     Constructor Create(AOwner: TComponent); Override;
     Destructor Destroy; Override;
   Published
-      { Published declarations }
-      (**
-       This property gets or sets the timing interval between information updates.
-       @precon  None.
-       @postcon Gets or sets the timing interval between information updates.
-       @return  an Integer
-       **)
+    { Published declarations }
+    (**
+      This property gets or sets the timing interval between information updates.
+      @precon  None.
+      @postcon Gets or sets the timing interval between information updates.
+      @return  an Integer
+    **)
     Property UpdateInterval: Integer Read FUpdateInterval Write SetUpdateInterval
       Default 5000;
-      (**
-       This property gets or set the back ground colour of the control.
-       @precon  None.
-       @postcon Gets or set the back ground colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the back ground colour of the control.
+      @precon  None.
+      @postcon Gets or set the back ground colour of the control.
+      @return  a TColor
+    **)
     Property BackColour: TColor Read FBackColour Write FBackColour Default clGray;
-      (**
-       This property gets or set the back ground font colour of the control.
-       @precon  None.
-       @postcon Gets or set the back ground font colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the back ground font colour of the control.
+      @precon  None.
+      @postcon Gets or set the back ground font colour of the control.
+      @return  a TColor
+    **)
     Property BackFontColour: TColor Read FBackFontColour Write FBackFontColour
       Default clBlack;
-      (**
-       This property gets or set the High Battery colour of the control.
-       @precon  None.
-       @postcon Gets or set the High Battery colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the High Battery colour of the control.
+      @precon  None.
+      @postcon Gets or set the High Battery colour of the control.
+      @return  a TColor
+    **)
     Property HighColour: TColor Read FHighColour Write FHighColour Default clRed;
-      (**
-       This property gets or set the Half Empty Battery colour of the control.
-       @precon  None.
-       @postcon Gets or set the Half Empty Battery colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the Half Empty Battery colour of the control.
+      @precon  None.
+      @postcon Gets or set the Half Empty Battery colour of the control.
+      @return  a TColor
+     **)
     Property HalfColour: TColor Read FHalfColour Write FHalfColour Default $00CCFF;
-      (**
-       This property gets or set the Low Battery colour of the control.
-       @precon  None.
-       @postcon Gets or set the Low Battery colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the Low Battery colour of the control.
+      @precon  None.
+      @postcon Gets or set the Low Battery colour of the control.
+      @return  a TColor
+     **)
     Property LowColour: TColor Read FLowColour Write FLowColour Default clLime;
-      (**
-       This property gets or set the High Battery Font colour of the control.
-       @precon  None.
-       @postcon Gets or set the High Battery Font colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the High Battery Font colour of the control.
+      @precon  None.
+      @postcon Gets or set the High Battery Font colour of the control.
+      @return  a TColor
+    **)
     Property HighFontColour: TColor Read FHighFontColour Write FHighFontColour
       Default clBlack;
-      (**
-       This property gets or set the Half Empty Battery Font colour of the control.
-       @precon  None.
-       @postcon Gets or set the Half Empty Battery Font colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the Half Empty Battery Font colour of the control.
+      @precon  None.
+      @postcon Gets or set the Half Empty Battery Font colour of the control.
+      @return  a TColor
+    **)
     Property HalfFontColour: TColor Read FHalfFontColour Write FHalfFontColour
       Default clBlack;
-      (**
-       This property gets or set the Low Battery Font colour of the control.
-       @precon  None.
-       @postcon Gets or set the Low Battery Font colour of the control.
-       @return  a TColor
-       **)
+    (**
+      This property gets or set the Low Battery Font colour of the control.
+      @precon  None.
+      @postcon Gets or set the Low Battery Font colour of the control.
+      @return  a TColor
+    **)
     Property LowFontColour: TColor Read FLowFontColour Write FLowFontColour
       Default clBlack;
-      (**
-       This property gets or set the Low Battery Point (Percentage) of the control.
-       @precon  None.
-       @postcon Gets or set the Low Battery Point (Percentage) of the control.
-       @return  a TPercentage
-       **)
+    (**
+      This property gets or set the Low Battery Point (Percentage) of the control.
+      @precon  None.
+      @postcon Gets or set the Low Battery Point (Percentage) of the control.
+      @return  a TPercentage
+    **)
     Property LowPoint: TPercentage Read FLowPoint Write SetLowPoint Default 10;
-      (**
-       This property gets or set the Half Empty Battery Point (Percentage) of the control.
-       @precon  None.
-       @postcon Gets or set the Half Empty Battery Point (Percentage) of the control.
-       @return  a TPercentage
-       **)
+    (**
+      This property gets or set the Half Empty Battery Point (Percentage) of the control.
+      @precon  None.
+      @postcon Gets or set the Half Empty Battery Point (Percentage) of the control.
+      @return  a TPercentage
+    **)
     Property HalfPoint: TPercentage Read FHalfPoint Write SetHalfPoint Default 50;
-      (**
-       This property gets or set the High Battery Point (Percentage) of the control.
-       @precon  None.
-       @postcon Gets or set the High Battery Point (Percentage) of the control.
-       @return  a TPercentage
-       **)
+    (**
+      This property gets or set the High Battery Point (Percentage) of the control.
+      @precon  None.
+      @postcon Gets or set the High Battery Point (Percentage) of the control.
+      @return  a TPercentage
+    **)
     Property HighPoint: TPercentage Read FHighPoint Write SetHighPoint Default 90;
-      (** Publish this existing property. @precon  None. @postcon None. **)
+    (** Publish this existing property. @precon  None. @postcon None. **)
     Property Align;
-      (** Publish this existing property. @precon  None. @postcon None. **)
+    (** Publish this existing property. @precon  None. @postcon None. **)
     Property BevelInner;
-      (** Publish this existing property. @precon  None. @postcon None. **)
+    (** Publish this existing property. @precon  None. @postcon None. **)
     Property BevelOuter;
-      (** Publish this existing property. @precon  None. @postcon None. **)
+    (** Publish this existing property. @precon  None. @postcon None. **)
     Property BevelWidth;
-      (** Publish this existing property. @precon  None. @postcon None. **)
+    (** Publish this existing property. @precon  None. @postcon None. **)
     Property BorderWidth;
-      (** Publish this existing property. @precon  None. @postcon None. **)
+    (** Publish this existing property. @precon  None. @postcon None. **)
     Property Font;
   End;
 
@@ -166,7 +166,8 @@ Implementation
 
 Uses
   Types,
-  dghlibrary;
+  dghlibrary,
+  Themes;
 
   (**
 
@@ -182,18 +183,18 @@ Begin
   RegisterComponents('DGH Controls', [TDGHMemoryMonitor]);
 End;
 
-  (**
+(**
 
-    This function calculates a shorter representation of a memory size and
-    returns a string representing the value.
+  This function calculates a shorter representation of a memory size and
+  returns a string representing the value.
 
-    @precon  None.
-    @postcon Returns a string representation of the given size.
+  @precon  None.
+  @postcon Returns a string representation of the given size.
 
-    @param   iSize as a Cardinal
-    @return  a String
+  @param   iSize as a Cardinal
+  @return  a String
 
-  **)
+**)
 Function CalcSize(iSize: Cardinal): String;
 
 Const
@@ -210,18 +211,18 @@ Begin
     Result := Format('%1.2nM', [Int(iSize) / dblMEGABYTE])
 End;
 
-  { TDGHBatteryMonitor }
+{ TDGHBatteryMonitor }
 
-  (**
+(**
 
-   This is the constructor method for the TDGHBatteryMonitor class.
+  This is the constructor method for the TDGHBatteryMonitor class.
 
-   @precon  None.
-   @postcon Creates and initialise the component including the timer.
+  @precon  None.
+  @postcon Creates and initialise the component including the timer.
 
-   @param   AOwner as a TComponent
+  @param   AOwner as a TComponent
 
-   **)
+**)
 Constructor TDGHMemoryMonitor.Create(AOwner: TComponent);
 
 Begin
@@ -247,31 +248,32 @@ Begin
   UpdateControl(Nil);
 End;
 
-  (**
+(**
 
-   This is the destructor method for the TDGHBatteryMonitor class.
+  This is the destructor method for the TDGHBatteryMonitor class.
 
-   @precon  None.
-   @postcon Disabled the timer and free's its mmemory.
+  @precon  None.
+  @postcon Disabled the timer and free's its mmemory.
 
-   **)
+**)
 Destructor TDGHMemoryMonitor.Destroy;
+
 Begin
   FTimer.Enabled := False;
   FTimer.Free;
   Inherited Destroy;
 End;
 
-  (**
+(**
 
-   This method sets the interval variables with information about the systems
-   power status.
+  This method sets the interval variables with information about the systems
+  power status.
 
-   @precon  None.
-   @postcon FBatteryStatus, FPercentage and FRemainingTime are set with the
-   battery status.
+  @precon  None.
+  @postcon FBatteryStatus, FPercentage and FRemainingTime are set with the
+           battery status.
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.GetFFMemoryManagerState;
 
 Var
@@ -294,14 +296,14 @@ Begin
     FPercentage := Trunc(Int(FUsed) / Int(FReserved) * 100);
 End;
 
-  (**
+(**
 
-   This is the controls overridden paint method.
+  This is the controls overridden paint method.
 
-   @precon  None.
-   @postcon Draw the display of battery information.
+  @precon  None.
+  @postcon Draw the display of battery information.
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.Paint;
 
 Const
@@ -333,7 +335,7 @@ Begin
   T.Top       := R.Top;
   T.Bottom    := R.Bottom;
   DrawText(Canvas.Handle, PChar(strStatus), Length(strStatus), T, DT_SINGLELINE Or
-      DT_VCENTER);
+    DT_VCENTER);
   // Render % of memory High in custom colour
   If FPercentage In [0 .. 100] Then
     Begin
@@ -356,32 +358,33 @@ Begin
         DT_VCENTER);
 End;
 
-  (**
+(**
 
-   This is a setter method for the LowPoint property.
+  This is a setter method for the LowPoint property.
 
-   @precon  None.
-   @postcon Sets the LowPoitn property.
+  @precon  None.
+  @postcon Sets the LowPoitn property.
 
-   @param   Value as a TPercentage as a constant
+  @param   Value as a TPercentage as a constant
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.SetLowPoint(Const Value: TPercentage);
+
 Begin
   If (Value < FHalfPoint) And (Value > 0) Then
     FLowPoint := Value;
 End;
 
-  (**
+(**
 
-   This is a setter method for the HighPoint property.
+  This is a setter method for the HighPoint property.
 
-   @precon  None
-   @postcon Sets the HighPoint property.
+  @precon  None
+  @postcon Sets the HighPoint property.
 
-   @param   Value as a TPercentage as a constant
+  @param   Value as a TPercentage as a constant
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.SetHighPoint(Const Value: TPercentage);
 
 Begin
@@ -389,49 +392,52 @@ Begin
     FHighPoint := Value;
 End;
 
-  (**
+(**
 
-   This is a setter method for the HalfPoint property.
+  This is a setter method for the HalfPoint property.
 
-   @precon  None.
-   @postcon Sets the HalfPoint Property
+  @precon  None.
+  @postcon Sets the HalfPoint Property
 
-   @param   Value as a TPercentage as a constant
+  @param   Value as a TPercentage as a constant
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.SetHalfPoint(Const Value: TPercentage);
+
 Begin
   If (Value > FLowPoint) And (Value < FHighPoint) Then
     FHalfPoint := Value;
 End;
 
-  (**
+(**
 
-   This is a setter method for the UpdateInterval property.
+  This is a setter method for the UpdateInterval property.
 
-   @precon  None.
-   @postcon Set the update interval of the componenot and the timer control.
+  @precon  None.
+  @postcon Set the update interval of the componenot and the timer control.
 
-   @param   Value as an Integer
+  @param   Value as an Integer
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.SetUpdateInterval(Value: Integer);
+
 Begin
   FUpdateInterval := Value;
   FTimer.Interval := Value;
 End;
 
-  (**
+(**
 
-   This method is an on timer event handler for the timer control.
+  This method is an on timer event handler for the timer control.
 
-   @precon  None.
-   @postcon Gets the battery status and then forces the control to re-paint.
+  @precon  None.
+  @postcon Gets the battery status and then forces the control to re-paint.
 
-   @param   Sender as a TObject
+  @param   Sender as a TObject
 
-   **)
+**)
 Procedure TDGHMemoryMonitor.UpdateControl(Sender: TObject);
+
 Begin
   GetFFMemoryManagerState;
   If CanInvalidate Then
