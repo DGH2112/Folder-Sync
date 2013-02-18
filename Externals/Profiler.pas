ae59feff17ef7ee23afe7b2ee8503044c336ec0c
DGH2112 <davidghoyle@gmail.com>
2013-02-18 20:42:02 +0000

    Updated the profiler to use a dynamic for and not be reliant on a form unit.

--- Externals/Profiler.pas
+++ Externals/Profiler.pas
@@ -4,7 +4,7 @@
   profiles.
 
   @Version 1.0
-  @Date    19 Sep 2010
+  @Date    18 Feb 2013
   @Author  David Hoyle
 
 **)
@@ -13,7 +13,10 @@ Unit Profiler;
 Interface
 
 Uses
-  Classes, Contnrs;
+  Classes,
+  Contnrs {$IFNDEF CONSOLE},
+  Forms,
+  StdCtrls {$ENDIF};
 
 Type
   (** This class represent a single element of profile information. It can
@@ -21,16 +24,16 @@ Type
       of the code being profiled. **)
   TProfile = Class
   Strict Private
-    FMethodName    : String;
-    FStartTick     : Double;
-    FDurationTick  : Double;
-    FInProcessTick : Double;
-    FCallCount     : Int64;
-    FProfiles      : TObjectList;
-    FParent        : TProfile;
-    FStackDepth    : Int64;
+    FMethodName   : String;
+    FStartTick    : Double;
+    FDurationTick : Double;
+    FInProcessTick: Double;
+    FCallCount    : Int64;
+    FProfiles     : TObjectList;
+    FParent       : TProfile;
+    FStackDepth   : Int64;
   Strict Protected
-    Function FindProfile(strMethodName : String; iStackDepth : Integer) : TProfile;
+    Function FindProfile(strMethodName: String; iStackDepth: Integer): TProfile;
     Procedure StartTiming;
     (**
       This property returns the total duration of the profiles calls, i.e. TickCount.
@@ -38,46 +41,55 @@ Type
       @postcon Returns the total duration of the profiles calls.
       @return  a Double
     **)
-    Property DurationTick : Double Read FDurationTick;
+    Property DurationTick: Double Read FDurationTick;
   Public
-    Constructor Create(strMethod : String; iStackDepth : Integer; objParent : TProfile);
+    Constructor Create(strMethod: String; iStackDepth: Integer; objParent: TProfile);
     Destructor Destroy; Override;
-    Function StartProfile(strMethodName : String; iStackDepth : Integer) : TProfile;
-    Function StopProfile : TProfile;
-    Procedure DumpProfileInformation(slProfileFile : TStringList);
+    Function StartProfile(strMethodName: String; iStackDepth: Integer): TProfile;
+    Function StopProfile: TProfile;
+    Procedure DumpProfileInformation(slProfileFile: TStringList);
     (**
       This property returns the number of Call Counts on the profile.
       @precon  None.
       @postcon Returns the number of Call Counts on the profile.
       @return  an Int64
     **)
-    Property CallCount : Int64 Read FCallCount;
+    Property CallCount: Int64 Read FCallCount;
   End;
 
   (** This class handles all the TProfile instances in a tree structure. **)
   TProfiler = Class
   Strict Private
-    FStackTop       : Integer;
-    FRootProfile    : TProfile;
-    FCurrentProfile : TProfile;
+    FStackTop      : Integer;
+    FRootProfile   : TProfile;
+    FCurrentProfile: TProfile;
+    {$IFNDEF CONSOLE}
+    FProgressForm  : TForm;
+    FLabel         : TLabel; 
+    {$ENDIF}
   Strict Protected
     Procedure DumpProfileInformation;
+    {$IFNDEF CONSOLE}
+    Procedure Msg(strMsg: String);
+    {$ENDIF}
   Public
     Constructor Create;
     Destructor Destroy; Override;
-    Procedure Start(strMethodName : String);
+    Procedure Start(strMethodName: String);
     Procedure Stop;
   End;
 
 Var
   (** This is a public variable which give the calling code access to the
       instance of the TPRofiler class. **)
-  CodeProfiler : TProfiler;
+  CodeProfiler: TProfiler;
 
 Implementation
 
 Uses
-  SysUtils, Windows {$IFNDEF CONSOLE}, ProgressForm {$ENDIF};
+  SysUtils,
+  Windows {$IFNDEF CONSOLE},
+  Controls {$ENDIF};
 
 (**
 
@@ -92,66 +104,76 @@ Uses
 Function TickTime: Double;
 
 Var
-  t, f : Int64;
-begin
+  t, f: Int64;
+Begin
   QueryPerformanceCounter(t);
   QueryPerformanceFrequency(f);
   Result := 1000.0 * Int(t) / Int(f);
-end;
+End;
 
 (**
 
-  This is a constructor for the TProfile class.
 
-  @precon  None.
-  @postcon Creates a container for the sub profiles.
+  This is a constructor for the TProfile class.
 
-  @param   strMethod   as a String
+
+  @precon  None.
+
+  @postcon Creates a container for the sub profiles.
+
+
+  @param   strMethod   as a String
   @param   iStackDepth as an Integer
   @param   objParent   as a TProfile
 
 **)
-constructor TProfile.Create(strMethod : String; iStackDepth : Integer;
-  objParent : TProfile);
+Constructor TProfile.Create(strMethod: String; iStackDepth: Integer;
+  objParent: TProfile);
 
-begin
+Begin
   FProfiles      := TObjectList.Create(True);
   FDurationTick  := 0;
   FInProcessTick := 0;
   FMethodName    := strMethod;
   FParent        := objParent;
   FStackDepth    := iStackDepth;
-end;
+End;
 
 (**
 
-  This is a destructor for the TProfile class.
+
+  This is a destructor for the TProfile class.
 
   @precon  None.
   @postcon Frees sub profiles.
 
-**)
-destructor TProfile.Destroy;
-begin
+
+**)
+Destructor TProfile.Destroy;
+Begin
   FProfiles.Free;
   Inherited Destroy;
-end;
+End;
 
 (**
 
-  This method outputs the profiles information to the given file handle.
 
-  @precon  None.
-  @postcon Outputs the profiles information to the given file handle.
+  This method outputs the profiles information to the given file handle.
 
-  @param   slProfileFile as a TStringList
+
+  @precon  None.
+
+  @postcon Outputs the profiles information to the given file handle.
+
+
+  @param   slProfileFile as a TStringList
 
 **)
-Procedure TProfile.DumpProfileInformation(slProfileFile : TStringList);
+Procedure TProfile.DumpProfileInformation(slProfileFile: TStringList);
 
 Var
-  i : Integer;
-  P : TProfile;
+  i   : Integer;
+  P   : TProfile;
   iPos: Integer;
 
 Begin
@@ -159,7 +181,7 @@ Begin
     Begin
       For i := 0 To FProfiles.Count - 1 Do
         Begin
-          P := FProfiles[i] As TProfile;
+          P              := FProfiles[i] As TProfile;
           FInProcessTick := FInProcessTick - P.DurationTick;
         End;
       iPos := Pos('.', FMethodName);
@@ -170,7 +192,7 @@ Begin
         FDurationTick,
         FInProcessTick,
         FCallCount
-      ]));
+        ]));
     End;
   For i := 0 To FProfiles.Count - 1 Do
     (FProfiles[i] As TProfile).DumpProfileInformation(slProfileFile);
@@ -192,10 +214,10 @@ End;
   @return  a TProfile
 
 **)
-Function TProfile.FindProfile(strMethodName : String; iStackDepth : Integer) : TProfile;
+Function TProfile.FindProfile(strMethodName: String; iStackDepth: Integer): TProfile;
 
 Var
-  i : Integer;
+  i: Integer;
   P: TProfile;
 
 Begin
@@ -226,7 +248,7 @@ End;
   @return  a TProfile
 
 **)
-Function TProfile.StartProfile(strMethodName : String; iStackDepth : Integer) : TProfile;
+Function TProfile.StartProfile(strMethodName: String; iStackDepth: Integer): TProfile;
 
 Begin
   Result := FindProfile(strMethodName, iStackDepth);
@@ -258,7 +280,7 @@ End;
   @return  a TProfile
 
 **)
-Function TProfile.StopProfile : TProfile;
+Function TProfile.StopProfile: TProfile;
 
 Begin
   FDurationTick  := FDurationTick + (TickTime - FStartTick);
@@ -273,142 +295,189 @@ End;
 
 (**
 
-  This is a constructor for the TProfiler class.
+
+  This is a constructor for the TProfiler class.
 
   @precon  None.
   @postcon Creates an initial TProfile instance for the tree structure.
 
-**)
-constructor TProfiler.Create;
-begin
+
+**)
+Constructor TProfiler.Create;
+
+Begin
+  {$IFNDEF CONSOLE}
+  FProgressForm               := TForm.Create(Nil);
+  FProgressForm.BorderStyle   := bsToolWindow;
+  FProgressForm.Caption       := 'Profiling...';
+  FProgressForm.ClientWidth   := 400;
+  FProgressForm.ClientHeight  := 50;
+  FProgressForm.Top           := Screen.Height - FProgressForm.Height;
+  FProgressForm.Left          := Screen.Width - FProgressForm.Width;
+  FProgressForm.FormStyle     := fsStayOnTop;
+  FProgressForm.Margins.Left  := 5;
+  FProgressForm.Margins.Right := 5;
+  FLabel                     := TLabel.Create(FProgressForm);
+  FLabel.Parent              := FProgressForm;
+  FLabel.Align               := alClient;
+  FLabel.Layout              := tlCenter;
+  FLabel.Caption             := 'Loading...';
+  FLabel.Font.Name           := 'Arial';
+  FLabel.Font.Size           := 9;
+  FLabel.WordWrap            := True;
+  FLabel.EllipsisPosition    := epEndEllipsis;
+  FProgressForm.Show;
+  Msg('Profiler started!');
+  {$ENDIF}
   FRootProfile := TProfile.Create('', 0, Nil);
-end;
+End;
 
 (**
 
-  This is a destructor for the TProfiler class.
+
+  This is a destructor for the TProfiler class.
 
   @precon  None.
   @postcon Frees the memory used by the profile tree.
 
-**)
-destructor TProfiler.Destroy;
-begin
+
+**)
+Destructor TProfiler.Destroy;
+Begin
   {$IFDEF PROFILECODE}
   DumpProfileInformation;
   {$ENDIF}
   FRootProfile.Free;
+  {$IFNDEF CONSOLE}
+  FProgressForm.Free;
+  {$ENDIF}
   Inherited Destroy;
-end;
+End;
 
 (**
 
-  This method starts the process of calculating and outputting the profile tree.
+
+  This method starts the process of calculating and outputting the profile tree.
 
   @precon  None.
   @postcon Starts the process of calculating and outputting the profile tree.
 
 **)
-procedure TProfiler.DumpProfileInformation;
+Procedure TProfiler.DumpProfileInformation;
 
 Var
-  strBuffer : Array[0..MAX_PATH] Of Char;
-  strModuleFileName, strFileName : String;
-  slProfile: TStringList;
-  {$IFNDEF CONSOLE}
-  frm : TfrmProgress;
-  {$ENDIF}
+  strBuffer                     : Array [0 .. MAX_PATH] Of Char;
+  strModuleFileName, strFileName: String;
+  slProfile                     : TStringList;
 
 Begin
-  {$IFNDEF CONSOLE}
-  frm := TfrmProgress.Create(Nil);
+  {$IFDEF CONSOLE}
+  WriteLn('Processing the profile information...');
+  {$ELSE}
+  Msg('Processing the profile information...');
   {$ENDIF}
+  GetModuleFileName(hInstance, strBuffer, MAX_PATH);
+  strFileName       := StrPas(strBuffer);
+  strModuleFileName := strFileName;
+  strFileName       := ChangeFileExt(strFileName, '.profile');
+  slProfile         := TStringList.Create;
   Try
-    {$IFDEF CONSOLE}
-    WriteLn('Processing the profile information...');
-    {$ELSE}
-    frm.Init(3, 'Profiling', 'Processing the profile information...');
-    {$ENDIF}
-    GetModuleFileName(hInstance, strBuffer, MAX_PATH);
-    strFileName := StrPas(strBuffer);
-    strModuleFileName := strFileName;
-    strFileName := ChangeFileExt(strFileName, '.profile');
-    slProfile := TStringList.Create;
-    Try
-      {$IFNDEF CONSOLE}
-      frm.UpdateProgress(1, 'Loading existing data...');
-      {$ELSE}
-      WriteLn('Loading existing data...');
-      {$ENDIF}
-      If FileExists(strFileName) Then
-        slProfile.LoadFromFile(strFileName);
-      slProfile.Add('Profile Dump For Application ' + strModuleFileName + ' on ' +
-        FormatDateTime('ddd dd/mmm/yyyy @ hh:mm:ss', Now));
-      slProfile.Add(Format('%s,%s,%s,%s,%s,%s', [
-        'Stack Depth',
-        'Class',
-        'Method Name',
-        'Total Tick Count (ms)',
-        'In Process Tick Count (ms)',
-        'Call Count'
-      ]));
-      {$IFNDEF CONSOLE}
-      frm.UpdateProgress(2, 'Processing new data...');
-      {$ELSE}
-      WriteLn('Loading existing data...');
-      {$ENDIF}
-      FRootProfile.DumpProfileInformation(slProfile);
-      {$IFNDEF CONSOLE}
-      frm.UpdateProgress(3, 'Saving data...');
-      {$ELSE}
-      WriteLn('Saving data...');
-      {$ENDIF}
-      slProfile.SaveToFile(strFileName);
-    Finally
-      slProfile.Free;
-    End;
-  Finally
     {$IFNDEF CONSOLE}
-    frm.Free;
+    Msg('Loading existing data...');
+    {$ELSE}
+    WriteLn('Loading existing data...');
     {$ENDIF}
+    If FileExists(strFileName) Then
+      slProfile.LoadFromFile(strFileName);
+    slProfile.Add('Profile Dump For Application ' + strModuleFileName + ' on ' +
+      FormatDateTime('ddd dd/mmm/yyyy @ hh:mm:ss', Now));
+    slProfile.Add(Format('%s,%s,%s,%s,%s,%s', [
+      'Stack Depth',
+      'Class',
+      'Method Name',
+      'Total Tick Count (ms)',
+      'In Process Tick Count (ms)',
+      'Call Count'
+      ]));
+    {$IFNDEF CONSOLE}
+    Msg('Processing new data...');
+    {$ELSE}
+    WriteLn('Loading existing data...');
+    {$ENDIF}
+    FRootProfile.DumpProfileInformation(slProfile);
+    {$IFNDEF CONSOLE}
+    Msg('Saving data...');
+    {$ELSE}
+    WriteLn('Saving data...');
+    {$ENDIF}
+    slProfile.SaveToFile(strFileName);
+  Finally
+    slProfile.Free;
   End;
-end;
+  Sleep(250);
+End;
+
+{$IFNDEF CONSOLE}
+(**
+
+  This method outputs a message to the profile form.
+
+  @precon  None.
+  @postcon Outputs a message to the profile form.
+
+  @param   strMsg as a String
+
+**)
+Procedure TProfiler.Msg(strMsg: String);
+
+Begin
+  FLabel.Caption := strMsg;
+  Application.ProcessMessages;
+End;
+{$ENDIF}
 
 (**
 
-  This method stops the profiling of the current method.
+
+  This method stops the profiling of the current method.
 
   @precon  None.
   @postcon Stops the profiling of the current method.
 
-**)
+
+**)
 Procedure TProfiler.Stop;
 
-begin
+Begin
   FStackTop := FStackTop - 1;
   If FStackTop < 0 Then
-    FStackTop := 0;
+    FStackTop     := 0;
   FCurrentProfile := FCurrentProfile.StopProfile;
-end;
+  If FStackTop <= 0 Then
+    Msg('Idle.');
+End;
 
 (**
 
-  This method starts the profiling of the current method.
+
+  This method starts the profiling of the current method.
 
   @precon  None.
   @postcon Starts the profiling of the current method.
 
-  @param   strMethodName as a String
+
+  @param   strMethodName as a String
 
 **)
-Procedure TProfiler.Start(strMethodName : String);
+Procedure TProfiler.Start(strMethodName: String);
 
-begin
+Begin
   If FStackTop = 0 Then
     FCurrentProfile := FRootProfile;
-  FStackTop := FStackTop + 1;
-  FCurrentProfile := FCurrentProfile.StartProfile(strMethodName, FStackTop);
-end;
+  FStackTop         := FStackTop + 1;
+  FCurrentProfile   := FCurrentProfile.StartProfile(strMethodName, FStackTop);
+  Msg('Profiling: ' + strMethodName);
+End;
 
 (** Creates the profiler on loading the module. **)
 Initialization
