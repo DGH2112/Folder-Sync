151730aea5e5e0bbe67902addd5058b134673868
DGH2112 <davidghoyle@gmail.com>
2016-04-17 15:49:28 +0100

    Fixed test issue.

--- Externals/TestDGHLibrary.pas
+++ Externals/TestDGHLibrary.pas
@@ -499,13 +499,13 @@ Var
 Begin
   strFileName := 'notepad.exe';
   Check(DGHFindOnPath(strFileName, ''), 'Check for notepad.exe');
-  CheckEquals('C:\Windows\system32\notepad.exe', strFileName);
+  CheckEquals('C:\Windows\notepad.exe', strFileName);
   strFileName := 'regedit.exe';
   Check(DGHFindOnPath(strFileName, ''), 'Check for regedit.exe');
-  CheckEquals('C:\Windows\system32\regedit.exe', strFileName);
+  CheckEquals('C:\Windows\regedit.exe', strFileName);
   strFileName := 'cmd.exe';
   Check(DGHFindOnPath(strFileName, ''), 'Check for cmd.exe');
-  CheckEquals('C:\Windows\system32\cmd.exe', strFileName);
+  CheckEquals('C:\Windows\System32\cmd.exe', strFileName);
 End;
 
 Procedure TestApplicationFunctions.TestDGHPathRelativePathTo;
